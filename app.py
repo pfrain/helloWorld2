cba33ee89fcb2d712b70587142d9b54b5833225d
pfrain <pfrain@terpmail.umd.edu>
2026-09-24 19:21:15 -0400

    added a new route to my html page about me

--- app.py
+++ app.py
@@ -11,6 +11,9 @@ def hello_world():
 def hello():
     return render_template('hello.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run()
